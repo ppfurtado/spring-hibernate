b33918b6c30fb8d0063efad9b380af5894a8421a
Pedro Furtado <pedro.nazare@pdcase.com.br>
2022-06-26 00:38:55 -0300

    first commit

---
+++ spring-demo-one/src/com/ppfurtadocode/springdemo/SetterDemoApp.java
@@ -0,0 +1,25 @@
+package com.ppfurtadocode.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SetterDemoApp {
+    public static void main(String[] args) {
+
+        //  Carregar o arquivo de configuração
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        //  recuperar bean do container spring
+        CricketCoach theCoach = context.getBean("myCricketCoah", CricketCoach.class);
+
+        // Chamando métodos do bean
+        System.out.println(theCoach.getDailyWorkout());
+
+        System.out.println(theCoach.getDailyFortune());
+
+
+        // fechando o context
+        context.close();
+
+    }
+
+}
