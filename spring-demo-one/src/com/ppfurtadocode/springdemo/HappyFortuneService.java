5af77d2d0e6b8037aadf8d1f830c0e3c76b27584
Pedro Furtado <pedro.nazare@pdcase.com.br>
2022-06-28 01:08:11 -0300

    feat: adicionar fortune de forma randomica

--- spring-demo-one/src/com/ppfurtadocode/springdemo/HappyFortuneService.java
+++ spring-demo-one/src/com/ppfurtadocode/springdemo/HappyFortuneService.java
@@ -1,9 +1,17 @@
 package com.ppfurtadocode.springdemo;
 
+import java.util.Random;
+
 public class HappyFortuneService implements FortuneService{
+
+    private int fortunes;
 
     @Override
     public String getFortune() {
-        return "Today is your lucky day!";
+        String[] f = {" without lucky", "with very lucky", "with lucky"};
+        Random gerador = new Random();
+        return "Today you is "+ f[gerador.nextInt(3)];
     }
+
+
 }
