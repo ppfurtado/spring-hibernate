cf3c254a01cc0ee88b96c7d1fec32e0bd7997a9e
Pedro Furtado <furtado.pedropaulo@gmail.com>
2022-06-30 07:00:50 -0300

    feat: adicionando arquivo com método main para testar como funciona o escopo de um bean

---
+++ spring-demo-one/src/com/ppfurtadocode/springdemo/BeanScopeDemoApp.java
@@ -0,0 +1,33 @@
+package com.ppfurtadocode.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanScopeDemoApp {
+    public static void main(String[] args) {
+
+
+//        carregar o arquivo de configuração  do spring
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
+
+//        carregar o bean no container
+        Coach theCoach = context.getBean("myCoach", Coach.class);
+
+        Coach alphaCoach = context.getBean("myCoach", Coach.class);
+
+//        verificar se os as intâncias do Coach são iguais
+
+        boolean result = (theCoach == alphaCoach);
+
+
+//        imprimir o result
+        System.out.println("\nResult: " + result);
+
+        System.out.println("\nMemória alocada para theCoach: " + theCoach);
+        System.out.println("\nMemória alocada para alphaCoach: " + alphaCoach);
+
+
+        // fechando  o context
+        context.close();
+    }
+
+}
