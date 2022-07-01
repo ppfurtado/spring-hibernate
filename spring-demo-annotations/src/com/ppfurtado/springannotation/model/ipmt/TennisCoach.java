713600576eb446519739a727f6c25ae82a7fb0e7
Pedro Furtado <furtado.pedropaulo@gmail.com>
2022-07-01 14:19:33 -0300

    feat: adicionando a anotação @Autorired em um propriedade da classe

--- spring-demo-annotations/src/com/ppfurtado/springannotation/model/ipmt/TennisCoach.java
+++ spring-demo-annotations/src/com/ppfurtado/springannotation/model/ipmt/TennisCoach.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach {
 
+    @Autowired
     private FortuneService fortuneService;
 
     public TennisCoach() {
@@ -21,11 +22,11 @@ public class TennisCoach implements Coach {
 //    }
 
 
-    @Autowired
-    public void doSomeCrazyStuff(FortuneService fortuneService) {
-        System.out.println(">> TennisCoach: inside  doSomeCrazyStuff method");
-        this.fortuneService = fortuneService;
-    }
+//    @Autowired
+//    public void doSomeCrazyStuff(FortuneService fortuneService) {
+//        System.out.println(">> TennisCoach: inside  doSomeCrazyStuff method");
+//        this.fortuneService = fortuneService;
+//    }
 
     @Override
     public String getDailyWorkout() {
