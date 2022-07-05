a0ebdf8064a9eee7c18b4e6a4f6824b0da22e3dc
Pedro Furtado <pedro.nazare@pdcase.com.br>
2022-07-05 07:32:18 -0300

    feat: criando novo coach para tester

---
+++ spring-demo-annotations/src/com/ppfurtado/springannotation/model/ipmt/SwimCoach.java
@@ -0,0 +1,38 @@
+package com.ppfurtado.springannotation.model.ipmt;
+
+import com.ppfurtado.springannotation.model.Coach;
+import com.ppfurtado.springannotation.service.FortuneService;
+import org.springframework.beans.factory.annotation.Value;
+
+public class SwimCoach implements Coach {
+
+    private FortuneService fortuneService;
+
+    @Value("${foo.email}")
+    private String email;
+
+    @Value("foo.team")
+    private String team;
+
+    public SwimCoach(FortuneService fortuneService) {
+        this.fortuneService = fortuneService;
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Swim 1000 meters as warmmm up";
+    }
+
+    @Override
+    public String getDailyFortune() {
+        return fortuneService.getFortune();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+}
