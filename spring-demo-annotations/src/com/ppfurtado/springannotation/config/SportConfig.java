cd35a73ac50f150f7696b2de5c62795128370488
Pedro Furtado <pedro.nazare@pdcase.com.br>
2022-07-05 00:14:39 -0300

    feat: adicionando classe de configuração

---
+++ spring-demo-annotations/src/com/ppfurtado/springannotation/config/SportConfig.java
@@ -0,0 +1,9 @@
+package com.ppfurtado.springannotation.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.ppfurtado.springannotation")
+public class SportConfig {
+}
