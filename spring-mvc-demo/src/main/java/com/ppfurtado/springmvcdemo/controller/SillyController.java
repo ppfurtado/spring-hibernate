e1c8d08689e04fb38f735b0b73c641c21034fcb9
Pedro Furtado <furtado.pedropaulo@gmail.com>
2022-07-09 08:04:58 -0300

    feat: adicionando classe e jsp silly

---
+++ spring-mvc-demo/src/main/java/com/ppfurtado/springmvcdemo/controller/SillyController.java
@@ -0,0 +1,14 @@
+package com.ppfurtado.springmvcdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/silly")
+public class SillyController {
+
+    @RequestMapping("/showForm")
+    public String displayTheForm(){
+        return "silly";
+    }
+}
