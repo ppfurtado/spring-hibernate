d79936f085fb830071268a276abcf38681a24159
Pedro Furtado <furtado.pedropaulo@gmail.com>
2022-07-09 00:41:46 -0300

    feat: adicionando controller root

---
+++ spring-mvc-demo/src/main/java/com/ppfurtado/springmvcdemo/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.ppfurtado.springmvcdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+
+    @RequestMapping("/")
+    public String showPage(){
+        return "main-view";
+    }
+}
