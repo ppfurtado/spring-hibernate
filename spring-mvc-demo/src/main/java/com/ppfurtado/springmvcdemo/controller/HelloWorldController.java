ad8e19f02001af0573add8c0787c79a68af5c35e
Pedro Furtado <pedro.nazare@pdcase.com.br>
2022-07-09 08:07:45 -0300

    feat: mapeando o paramentro pela anotação @RequestParam

--- spring-mvc-demo/src/main/java/com/ppfurtado/springmvcdemo/controller/HelloWorldController.java
+++ spring-mvc-demo/src/main/java/com/ppfurtado/springmvcdemo/controller/HelloWorldController.java
@@ -3,11 +3,13 @@ package com.ppfurtado.springmvcdemo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Locale;
 
 @Controller
+@RequestMapping("/hello")
 public class HelloWorldController {
 
 
@@ -39,6 +41,23 @@ public class HelloWorldController {
         // retornando o jsp
         return "helloworld";
     }
+
+    @RequestMapping("/processFormVersionThree")
+    public String processFormVersionThree(@RequestParam("studentName") String theName, Model model){
+
+        // Converter o texto para caixa alta
+        theName = theName.toUpperCase(Locale.ROOT);
+
+        // criando a mensagem
+        String result = "Hey my good, " + theName;
+
+        // adicioando a mensagem  no model
+        model.addAttribute("message", result);
+
+        // retornando o jsp
+        return "helloworld";
+    }
+
 
 
 }
